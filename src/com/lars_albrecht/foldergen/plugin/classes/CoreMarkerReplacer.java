c8f25c7a6b6cc8612b61fd7b652cd42f850745c7
lars.albrecht <lars.albrecht@edab17fb-6e08-1416-a1a3-431e2f12119e>
2011-07-20 17:27:47 +0000

    added Version 1.5.0.0 -pre1

---
+++ src/com/lars_albrecht/foldergen/plugin/classes/CoreMarkerReplacer.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2011 Lars Chr. Albrecht
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the project's author nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * 
+ */
+package com.lars_albrecht.foldergen.plugin.classes;
+
+/**
+ * Contains the plugin to replace.
+ * 
+ * @author lalbrecht
+ * @version 1.0.0.0
+ */
+public abstract class CoreMarkerReplacer {
+	/**
+	 * The content to replace.
+	 */
+	protected String content;
+	protected String name;
+
+	/**
+	 * @return the content
+	 */
+	public synchronized final String getContent() {
+		return this.content;
+	}
+
+	/**
+	 * @param content
+	 *            the content to set
+	 */
+	public synchronized final void setContent(final String content) {
+		this.content = content;
+	}
+
+	/**
+	 * Returns the name.
+	 * 
+	 * @return String
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * Replaces the content.
+	 * 
+	 * @return String
+	 */
+	public String replaceContent() {
+		return null;
+	}
+}
